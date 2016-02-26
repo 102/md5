cae81ca5a8a7d25e2d2d078b4966332dba7456e5
Roman Gusev <rgusev@hwdtech.com>
2016-02-26 17:32:26 +0600

    Initial commit

---
+++ hash.py
@@ -0,0 +1,2 @@
+def md5(string):
+  return 'd41d8cd98f00b204e9800998ecf8427e'
---
+++ test.py
@@ -0,0 +1,10 @@
+import unittest
+import hash
+
+class TestMD5(unittest.TestCase):
+
+  def test_empty(self):
+    self.assertEqual(hash.md5(''), 'd41d8cd98f00b204e9800998ecf8427e')
+
+if __name__ == '__main__':
+  unittest.main()
