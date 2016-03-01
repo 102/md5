471bdc4fed22359053e423d198b71e61af82a606
Roman Gusev <rgusev@hwdtech.com>
2016-03-01 17:26:13 +0600

    Fix formatting

--- hash.py
+++ hash.py
@@ -1,2 +1,2 @@
 def md5(string):
-  return 'd41d8cd98f00b204e9800998ecf8427e'
+    return 'd41d8cd98f00b204e9800998ecf8427e'
--- test.py
+++ test.py
@@ -1,10 +1,11 @@
 import unittest
 import hash
 
-class TestMD5(unittest.TestCase):
 
-  def test_empty(self):
-    self.assertEqual(hash.md5(''), 'd41d8cd98f00b204e9800998ecf8427e')
+class TestMD5(unittest.TestCase):
+    def testEmpty(self):
+        self.assertEqual(hash.md5(''), 'd41d8cd98f00b204e9800998ecf8427e')
+
 
 if __name__ == '__main__':
-  unittest.main()
+    unittest.main()
